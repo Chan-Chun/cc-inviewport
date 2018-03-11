cf739de9e458271ade3d4e11e9d9179faa934296
Chan-Chun <362732414@qq.com>
2018-03-11 23:42:47 +0800

    rewrite packing

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: path.join(__dirname, 'src', 'index.js'),
   output: {
     filename: 'cc-inviewport.js',
-    path: path.join(__dirname, "dist"),
+    path: path.join(__dirname, "build"),
     library: name
   },
   module: {
