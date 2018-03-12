f6f234074f9fb2e2c71874d5d44b11b457cc3afa
Chan-Chun <362732414@qq.com>
2018-03-12 11:59:31 +0800

    minify

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 module.exports = (element, offset = 0) => {
-  if (!element && !window) {
+  if (!window) {
     throw new Error('Please running in browser')
+    return
+  }
+  if (!element) {
+    throw new Error('Element does not exist')
     return
   }
   if (typeof offset !== 'number') {
     throw new Error('Offset is\'t number')
     return
   }
-  let mainOffsetTop = element.offsetTop
-  let mainHeight = element.offsetHeight
-  let winHeight = window.innerHeight
-  let winScrollTop = window.document.body.scrollTop + window.document.documentElement.scrollTop
+  const mainOffsetTop = element.offsetTop
+  const mainHeight = element.offsetHeight
+  const winHeight = window.innerHeight
+  const winScrollTop = window.document.body.scrollTop + window.document.documentElement.scrollTop
   return !(winScrollTop > mainOffsetTop + mainHeight + offset || winScrollTop + offset < mainOffsetTop - winHeight)
 }
